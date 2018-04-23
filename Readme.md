9cbfd2b076d1802cd582783ef96c01db393a2350
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to Assign a ComboBox Editor to a Column
+
+
+<p>This example shows how to assign the combo box and spin editors to grid columns.</p><br />
+
+
+<br/>
+
+
---
+++ VB/AssignComboBoxEditToColmn/MainPage.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Editors.Settings
+
+Namespace AssignComboBoxEditToColmn
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+			CType(grid.Columns("ProductName").EditSettings, ComboBoxEditSettings).ItemsSource = New ProductList()
+		End Sub
+	End Class
+End Namespace
