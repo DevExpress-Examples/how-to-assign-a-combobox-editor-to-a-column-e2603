e8a472dc983823201f73fe1e1a42e622f2a1bf14
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AssignComboBoxEditToColmn/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Editors.Settings;
+
+namespace AssignComboBoxEditToColmn {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+            ((ComboBoxEditSettings)grid.Columns["ProductName"].EditSettings).ItemsSource = new ProductList();
+        }
+    }
+}
